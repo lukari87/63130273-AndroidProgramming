84c0f7c7d14d447cd34d7626e528b6bcf396d102
Lukari <115005567+lukari87@users.noreply.github.com>
2024-03-25 14:14:01 +0700

    Update MainActivity.java

--- Cau3_GioiThieu/app/src/main/java/ntu_63134338/cau3_gioithieu/MainActivity.java
+++ Cau3_GioiThieu/app/src/main/java/ntu_63134338/cau3_gioithieu/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_);
         ImageView img = findViewById(R.id.imageView);
         img.setImageResource(R.mipmap.avt);
     }
