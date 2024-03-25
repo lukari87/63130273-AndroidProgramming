a544da6f46abbed791de047b5eda581d6734c8a4
Lukari <115005567+lukari87@users.noreply.github.com>
2024-03-25 12:17:36 +0700

    Update MainActivity.java

--- Cau2_CurChange/app/src/main/java/ntu_63130273/cau2_curchange/MainActivity.java
+++ Cau2_CurChange/app/src/main/java/ntu_63130273/cau2_curchange/MainActivity.java
@@ -1,24 +1,19 @@
 package ntu_63130273.cau2_curchange;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
-}
+    //Create objects for all the widgets
+    TextView tv_to,tv_from,tv_result;
+    EditText edt_amount;
+    Button btnusd,btngbp,btnaus,btncan,btneuro,btnjpy;
+    String amount;
+    int pkr,result;
